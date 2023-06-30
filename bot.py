5249fd5e23cc1097907e767675beed36de843407
saftman <80041584+saftman@users.noreply.github.com>
2023-06-30 18:21:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,6 +64,75 @@ async def send_server_info():
     await channel.send(embed=embed)
 
 
+async def send_player_info(player):
+    guild = client.get_guild(int(GUILD_ID))
+    channel = guild.get_channel(int(CHANNEL_ID))
+
+    player_info = get_player_info(player)
+
+    embed = discord.Embed(title='Player Information', color=discord.Color.green())
+    embed.set_thumbnail(url=player_info['skin_url'])
+    embed.add_field(name='Player Name', value=player, inline=True)
+    embed.add_field(name='Last Online', value=player_info['last_online'], inline=True)
+    embed.add_field(name='Playtime', value=f"{player_info['playtime']} minutes", inline=True)
+    if player_info['online_status']:
+        embed.add_field(name='Status', value='Online', inline=True)
+    else:
+        embed.add_field(name='Status', value='Offline', inline=True)
+
+    await channel.send(embed=embed)
+
+
+async def send_top_playtime():
+    guild = client.get_guild(int(GUILD_ID))
+    channel = guild.get_channel(int(CHANNEL_ID))
+
+    filename = 'playertime.txt'
+
+    try:
+        with open(filename, 'r') as file:
+            data = file.readlines()
+            data = [line.strip().split(':') for line in data]
+            data = sorted(data, key=lambda x: int(x[1]), reverse=True)
+            top_players = data[:10]
+
+            embed = discord.Embed(title='Top 10 Players by Playtime', color=discord.Color.gold())
+            for i, player in enumerate(top_players):
+                player_name, playtime = player
+                embed.add_field(name=f'#{i + 1} {player_name}', value=f'Playtime: {playtime} minutes', inline=False)
+
+            await channel.send(embed=embed)
+    except FileNotFoundError:
+        await channel.send('No playtime data available.')
+
+
+def get_player_info(player):
+    filename = 'playertime.txt'
+
+    try:
+        with open(filename, 'r') as file:
+            data = file.readlines()
+            for line in data:
+                line = line.strip().split(':')
+                if line[0] == player:
+                    playtime = int(line[1])
+                    return {
+                        'playtime': playtime,
+                        'last_online': line[2],
+                        'online_status': line[3] == 'online',
+                        'skin_url': f"https://mc-heads.net/avatar/{player}"
+                    }
+    except FileNotFoundError:
+        pass
+
+    return {
+        'playtime': 0,
+        'last_online': 'N/A',
+        'online_status': False,
+        'skin_url': f"https://mc-heads.net/avatar/{player}"
+    }
+
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name} ({client.user.id})')
@@ -74,6 +143,14 @@ async def on_ready():
 async def on_message(message):
     if message.content == '!info':
         await send_server_info()
+    elif message.content.startswith('!player'):
+        player = message.content.split(' ')[-1]
+        if player:
+            await send_player_info(player)
+        else:
+            await message.channel.send('Please provide a player name.')
+    elif message.content == '!top':
+        await send_top_playtime()
 
 
 @client.event
